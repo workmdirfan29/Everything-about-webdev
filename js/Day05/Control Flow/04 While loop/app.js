243773dff62606acac164e04f97819a9acaf5c6f
Md Irfan <work.mdirfan@gmail.com>
2025-03-08 21:45:05 +0530

    While loop

---
+++ js/Day05/Control Flow/04 While loop/app.js
@@ -0,0 +1,18 @@
+// while (condition) {
+//   // code block to be executed
+// }
+
+
+let i = 1;
+
+while (i <= 5) {    // Loop will run as long as i is less than or equal to 5
+  console.log(i);  // Print the value of i
+  i++;
+}
+
+//<----------------------------------->
+let j=1;
+while(j <= 10){
+    console.log(j,"JAVA SCRIPT");
+    j++;
+}
