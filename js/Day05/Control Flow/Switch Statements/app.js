f45751c93fdca96195487748dbcde9a853b3396f
Md Irfan <work.mdirfan@gmail.com>
2025-03-08 19:32:20 +0530

    Switch Statments

---
+++ js/Day05/Control Flow/Switch Statements/app.js
@@ -0,0 +1,51 @@
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+//<--------------------------------------->
+let x = 1;
+
+switch (x) {
+  case 0:
+    bulb = "Off";
+    console.log(bulb)
+    break;
+  case 1:
+    bulb = "On";
+    console.log(bulb)
+
+    break;
+  default:
+    bulb = "No value found";
+    console.log(bulb)
+
+}
+//<------------------------------------------>
+
+
+fruit = "orange";
+
+switch(fruit)  {
+    case "banana":
+        console.log("Banana is good");
+        break;
+    case "orange":
+
+        console.log("I am not a fan of orange");
+        break;
+
+    case "apple":
+        console.log("how you like them apple");
+        break;
+    default:
+        console.log("I have never hard of that fruit");
+       
+}
+
