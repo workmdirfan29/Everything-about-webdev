ed32a135770ec0078e85ee142b0c83dbda1aa071
Md Irfan <work.mdirfan@gmail.com>
2025-03-08 21:52:09 +0530

    Do-While loop

---
+++ js/Day05/Control Flow/Do-While loop/app.js
@@ -0,0 +1,19 @@
+//  do {
+    // code block to be executed
+//  } while (condition);
+
+let i = 1;  // Initialize the variable i
+
+do {
+  console.log(i);   // Print the value of i
+  i++;              // Increment i by 1
+} while (i <= 5);   // Loop continues as long as i is less than or equal to 5
+
+//<------------------------------------------->
+
+let j=1;
+do{
+    console.log(j,"JAVA SCRIPT");
+    j++;
+}
+while(j<=10);
