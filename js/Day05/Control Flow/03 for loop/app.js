e26923faf457eab8964d9441969cab859d20983a
Md Irfan <work.mdirfan@gmail.com>
2025-03-08 20:03:49 +0530

    For Loop

---
+++ js/Day05/Control Flow/03 for loop/app.js
@@ -0,0 +1,11 @@
+// for (initialExpression; condition; incrementExpression) {}
+
+// <------------------for loop------------------->
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i,"Hello World");
+  }
+//  <-------------------------------->
+  for(let j = 0; j <= 10; j++){
+    console.log(j,"JAVA SCRIPT");
+  }
