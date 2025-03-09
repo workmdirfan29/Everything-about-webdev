47033fe18a939ef8ca021fc08edce9c7778ae38f
Md Irfan <work.mdirfan@gmail.com>
2025-03-09 18:45:58 +0530

    Declaration && Expression

---
+++ js/Day05/Functions/02 Declaration  &&  Exression/app.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function sayHello(username) {
+    console.log(`Hello ${username}`);
+  }
+  
+  sayHello("Jhon Wick");
+  
+  // Function Expressions
+  const greetings = function (user) {
+    console.log(`Hello ${user}`);
+  };
+  
+  greetings("JAVA SCRIPT");
+  
+  // -------------------------
+  console.log(x); // ERROR
+  let x = 10;
